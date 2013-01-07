5913642676c1594c90e4ffd5f33d0e1912c8666c
Joao Vortmann <jvortmann@gmail.com>
2013-01-07 16:05:34 -0200

    Forces tinymce to be local.

--- app/assets/javascripts/monologue/admin/tinymce-config.js
+++ app/assets/javascripts/monologue/admin/tinymce-config.js
@@ -1,3 +1,9 @@
+window.tinyMCEPreInit = window.tinyMCEPreInit || {
+  base:   '/assets/tinymce',
+  query:  '3.5.6',
+  suffix: ''
+};
+
 $(function() {
   $('textarea').tinymce({
     theme: 'advanced',
